d5c322d4d8a7868e2173e2b172ce172f2cb88aff
Gallopt <accapim@outlook.com>
2021-03-04 13:56:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 int main(int argc, char *argv[])
 {
 #ifdef QT_DEBUG
-    qSetMessagePattern("[%{time hh:mm:ss.zzz}]%{file} %{if-debug}D%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}%{if-fatal}F%{endif}"
-                       ":%{line}\r\n                                       ---->%{message}\r");
+    qSetMessagePattern("[%{time hh:mm:ss.zzz}]%{file} %{if-debug}%{endif}%{if-warning}W%{endif}"
+                       "%{if-critical}C%{endif}%{if-fatal}F%{endif}"
+                       "%{file}:%{line}\n"
+"                                     ---->%{message}\r");
 #endif
     QApplication a(argc, argv);
     Painter w;
